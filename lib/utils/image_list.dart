8bf6dbadaa74a7c7b8ebd5c77f604664a9267aa7
Admin <drashtipatel7080@gmail.com>
2024-05-31 20:04:56 +0530

    Initial commit

---
+++ lib/utils/image_list.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+List<Map<String, dynamic>> imgList = [
+  {
+    'title' : 'Nature',
+    'img' : 'assets/img/nature.jpg',
+    'text' : 'Nature refers to physical world and all forms of life species on earth in general.',
+    'color' : [
+      Colors.green,
+      Colors.green.shade200,
+      Colors.green.shade200,
+      Colors.green.shade100,
+    ],
+  },
+
+  {
+    'title' : 'Nature',
+    'img' : 'assets/img/flwer.avif',
+    'text' : 'Therefore, flowers are an essential part of our lives.',
+    'color' : [
+      Colors.purple,
+      Colors.purple.shade200,
+      Colors.purple.shade200,
+      Colors.purple.shade100,
+    ],
+  },
+
+  {
+    'title' : 'Nature',
+    'img' : 'assets/img/bird.jpeg',
+    'text' : 'Birds are very special animals that have particular characteristics.',
+    'color' : [
+      Colors.amber,
+      Colors.amber.shade200,
+      Colors.amber.shade200,
+      Colors.amber.shade100,
+    ],
+  },
+
+  {
+    'title' : 'Nature',
+    'img' : 'assets/img/yoga.avif',
+    'text' : 'Yoga is a Sanskrit word meaning to meet or be united.',
+    'color' : [
+      Colors.pink,
+      Colors.pink.shade200,
+      Colors.pink.shade200,
+      Colors.pink.shade100,
+    ],
+  },
+];
