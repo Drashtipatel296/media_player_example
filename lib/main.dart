b8950ee3e10b422bbf54c6f1e8b13e3dd87d0980
Admin <drashtipatel7080@gmail.com>
2024-06-02 20:17:58 +0530

    02-06-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:media_player_example/provider/slider_provider.dart';
+import 'package:media_player_example/screens/Parallax%20Effect%20in%20Flutter/effect_screen.dart';
 import 'package:provider/provider.dart';
 import 'screens/home_screen.dart';
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: EffectScreen(),
     );
   }
 }
---
+++ lib/utils/video_list.dart
@@ -0,0 +1,7 @@
+final videos = [
+  'assets/video/beach.mp4',
+  'assets/video/city.mp4',
+  'assets/video/coral.mp4',
+  'assets/video/flower.mp4',
+  'assets/video/mountain.mp4',
+];
